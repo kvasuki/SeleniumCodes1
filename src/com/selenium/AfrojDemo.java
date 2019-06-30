13b096839158b36f2cb929cebf58cbcfc6397419
afroj <safroj7@gmail.com>
2019-06-30 12:55:50 +0530

    new

---
+++ src/com/selenium/AfrojDemo.java
@@ -0,0 +1,11 @@
+package com.selenium;
+
+public class AfrojDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hi.................");
+	}
+
+}
